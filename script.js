aad7c93201071319338af35eadc8fbf15366f6fe
Nickolas Menotti <nmenotti@cox.net>
2020-10-31 22:08:33 -0700

    Completed README.md, and tinkered more with script.js to try to get local storage working properly.

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ setInterval(() => {
 var timeBlock = $(".time-block");
 var plan = $("textarea");
 
-//**************************************************************************************************************************************************************/
 //Pass arguments to change the color of the timeBlock based on the current time.
 //If the current moment is less than the timeBlock ID, then make the color "past"
 for (var i = 0; i < timeBlock.length; i++) {
@@ -30,7 +29,6 @@ else if (parseInt(timeBlock[i].id) > moment ().format("H")) {
     }
 };
 
-//*************************************************************************************************************************************************************/
 //Save tasks in local storage when save button in clicked
 
 // Create a variable for the time slot array
@@ -49,14 +47,12 @@ timeSlotArray.each(function(timeSlotIndex)  {
 for (let i = 9; i < timeSlotArray.length + 9; i++) {
     //check if null
     var storedData = localStorage.getItem(`hour-${i}`);
-    console.log("*******", storedData);
-    if (storedData) {
+    console.log(id = i, );
         //add the text to the right spot
         $(`[data=${i}]`).find("textarea").val( storedData);
     }
-}
 
-//add event listender to save button
+//add event listener to save button
 $(".save-button").on("click",saveNotes);
 
 //save data
@@ -70,32 +66,27 @@ function saveNotes(event) {
 };
 
 //GET LOCAL STORAGE DATA and populatre the textarea
-// var timeSlotArray = $("time-slot");
+for (let i = 9; i < timeSlotArray.length; +9, i++) {
 
-// for (let i = 9; i < timeSlotArray.length; +9, i++) {
-
-//     // Check if null
-//     var storedData = localStorage.getItem('data${i}');
-//     if (storedData) {
+    // Check if null
+    var storedData = localStorage.getItem('data${i}');
+    if (storedData) {
     
-//     // Add the text to the right spot    
-//     $('[data=${i}]').find("textarea").val(
-//         localStorage.getItem('data${i}'));
-//     }    
-// }
+    // Add the text to the right spot    
+    $('[data=${i}]').find("textarea").val(
+        localStorage.getItem('data${i}'));
+    }    
+}
 
-// // Add eventListener to save button
-$(".save-button").on("click, saveNotes");
+// Save data
+function saveNotes(event) {
 
-// // Save data
-// function saveNotes(event) {
-//     console.log("*******this", this);
-//     // Get the data
-//     var timeText = $(event.target).siblings("textarea").val();
+    // Get the data
+    var timeText = $(event.target).siblings("textarea").val();
     
-//     // Get the ID 
-//     var id = $(event.target).closest("time-slot").attr("data");
+    // Get the ID 
+    var id = $(event.target).closest("time-slot").attr("data");
     
-//     // Store data
-//     localStorage.setItem(`data${id}`,timeText);
-// }
+    // Store data
+    localStorage.setItem(`data${id}`,timeText);
+}
